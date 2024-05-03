fa41f7049a9ad280c41dfa5e9dedaad6ef96c245
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-05-02 20:55:35 -0600

    configure tmux
    Nice to have plugins and everything taken care of
    Might have to tweak this more as I get used to it
    But this works for now

--- home.nix
+++ home.nix
@@ -21,7 +21,6 @@
   # environment.
   home.packages = [
     pkgs.fish
-    pkgs.tmux
     pkgs.starship
     pkgs.neovim
     pkgs.curl
@@ -52,6 +51,39 @@
     # '')
   ];
 
+  programs.tmux = {
+    enable = true;
+    sensibleOnTop = false;
+    shortcut = "a";
+    baseIndex = 1;
+    shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    secureSocket = false;
+    escapeTime = 0;
+    plugins = with pkgs;
+      [
+        tmuxPlugins.better-mouse-mode
+        tmuxPlugins.yank
+        tmuxPlugins.vim-tmux-navigator
+        tmuxPlugins.nord
+      ];
+    extraConfig = ''
+      set -ga terminal-overrides ",screen-256color*:Tc"
+      set-option -g default-terminal "screen-256color"
+      set-option -g mouse on
+      set -g status-position top
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      # vim-like pane switching
+      bind -r ^ last-window
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
+    '';
+  };
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
