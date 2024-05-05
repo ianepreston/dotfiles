5106cf5417907316aa27decacdb5137475ed3577
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-05-05 12:17:42 -0600

    added neo-tree
    Also ended up running auto format

--- neovim/lua/plugins/neo-tree.lua
+++ neovim/lua/plugins/neo-tree.lua
@@ -2,22 +2,32 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
   },
-  cmd = 'Neotree',
+  cmd = "Neotree",
   keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    {
+      "<leader>n",
+      function()
+        require("neo-tree.command").execute({
+          toggle = true,
+          source = "filesystem",
+          position = "current",
+        })
+      end,
+      desc = "filesystem (root dir)",
+    },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ["\\"] = "close_window",
         },
       },
     },
