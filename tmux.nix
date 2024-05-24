17480120e5eb361bc42e9a14cf575c88bedc5a64
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-05-24 17:14:30 -0600

    refactor tmux to own file
    Mostly just to see if I have my head around this stuff

---
+++ tmux.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+
+  programs.tmux = {
+    enable = true;
+    sensibleOnTop = false;
+    shortcut = "a";
+    baseIndex = 1;
+    shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
+    secureSocket = false;
+    escapeTime = 0;
+    plugins = with pkgs;
+      [
+        tmuxPlugins.better-mouse-mode
+        tmuxPlugins.yank
+        tmuxPlugins.vim-tmux-navigator
+        tmuxPlugins.nord
+      ];
+    extraConfig = ''
+      set -ga terminal-overrides ",screen-256color*:Tc"
+      set-option -g default-terminal "screen-256color"
+      set-option -g mouse on
+      set -g status-position top
+      # easy-to-remember split pane commands
+      bind | split-window -h -c "#{pane_current_path}"
+      bind - split-window -v -c "#{pane_current_path}"
+      # vim-like pane switching
+      bind -r ^ last-window
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
+    '';
+  };
+}
