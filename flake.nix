1d04aabdaa9116745339e3ec4b10ed2611809f91
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-06-22 16:57:20 -0600

    also as root in devcontainer

--- flake.nix
+++ flake.nix
@@ -27,6 +27,17 @@
           # to pass through arguments to home.nix
         };
       # Also create one for if I'm in a devcontainer
+      homeConfigurations."root" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ./tmux.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
       homeConfigurations."vscode" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
