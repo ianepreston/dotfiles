2966b69c444576692f727ac4f926446c8a4842f3
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-06-23 08:36:58 -0600

    add dotfiles back in

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./vscode.nix ./home.nix ./packages.nix ];
+        modules = [ ./vscode.nix ./home.nix ./packages.nix ./dotfiles.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
