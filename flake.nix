c291fe5f4023e79f7a965b147de2d42b27840858
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-06-23 08:35:05 -0600

    add packages back in to devcontainer user

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [ ./vscode.nix ./home.nix ];
+        modules = [ ./vscode.nix ./home.nix ./packages.nix ];
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
