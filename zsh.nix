7a3de58fba486acb8bc38c2154a0641217752180
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-06-24 08:03:49 -0600

    try ssh-agent instead of keychain
    This didn't inject properly into the container, it made a new agent

--- zsh.nix
+++ zsh.nix
@@ -17,15 +17,24 @@
       eval "$(starship init zsh)"
       alias vim="nvim"
       alias vi="nvim"
+      if [ -z "$SSH_AUTH_SOCK" ]; then
+         # Check for a currently running instance of the agent
+         RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+         if [ "$RUNNING_AGENT" = "0" ]; then
+              # Launch a new instance of the agent
+              ssh-agent -s &> $HOME/.ssh/ssh-agent
+         fi
+         eval `cat $HOME/.ssh/ssh-agent`
+      fi
     '';
-    oh-my-zsh = {
-      enable = true;
-      extraConfig = ''
-        zstyle :omz:plugins:keychain agents gpg,ssh
-        zstyle :omz:plugins:keychain identities id_ed25519 id_rsa
-        zstyle :omz:plugins:keychain options --quiet --ignore-missing -Q
-      '';
-      plugins = [ "keychain" ];
-    };
+    # oh-my-zsh = {
+    #   enable = true;
+    #   extraConfig = ''
+    #     zstyle :omz:plugins:keychain agents gpg,ssh
+    #     zstyle :omz:plugins:keychain identities id_ed25519 id_rsa
+    #     zstyle :omz:plugins:keychain options --quiet --ignore-missing -Q
+    #   '';
+    #   plugins = [ "keychain" ];
+    # };
   };
 }
