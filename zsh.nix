e003c400d8ba1d6c13710db45626821627bf89fb
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-06-24 07:48:37 -0600

    add alias for vim and keychain plugin

--- zsh.nix
+++ zsh.nix
@@ -15,6 +15,17 @@
     };
     initExtra = ''
       eval "$(starship init zsh)"
+      alias vim="nvim"
+      alias vi="nvim"
     '';
+    oh-my-zsh = {
+      enable = true;
+      extraConfig = ''
+        zstyle :omz:plugins:keychain agents gpg,ssh
+        zstyle :omz:plugins:keychain identities id_ed25519 id_rsa
+        zstyle :omz:plugins:keychain options --quiet --ignore-missing -Q
+      '';
+      plugins = [ "keychain" ];
+    };
   };
 }
