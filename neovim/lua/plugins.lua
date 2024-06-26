b67da9a861cc8a52da2f55f2c51b275cfb933421
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-06-26 08:40:18 -0600

    add scrolleof
    keep some whitespace at the bottom of text you're editing if you're at
    the end of file

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -52,6 +52,7 @@ require("lazy").setup({
   require("plugins/todo-comments"),
 
   require("plugins/mini"),
+  require("plugins/scrolleof"),
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
