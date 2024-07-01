eb176bb5ec1211e8dfdf7c16e353d561e976660d
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-07-01 15:36:17 -0600

    directly import plugins folder
    doing a require for each file was hacky

--- neovim/init.lua
+++ neovim/init.lua
@@ -17,7 +17,7 @@ require("keymaps")
 require("lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
-require("plugins")
+require("lazy").setup("plugins")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
