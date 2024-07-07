3b4997133948c06497f7c58e91bd60a3dcae51da
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2024-07-07 16:25:42 -0600

    switch for ruff format

--- neovim/lua/plugins/conform.lua
+++ neovim/lua/plugins/conform.lua
@@ -28,8 +28,7 @@ return {
         lua = { "stylua" },
         nix = { "nixfmt" },
         -- Conform can also run multiple formatters sequentially
-        python = { "isort", "black" },
-        --
+        python = { "isort", "ruff_format" },
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
